57d5d6de0710a7c50557c7824807f64f03bdde04
Evandro2270 <evandro535@gmail.com>
2024-12-04 09:22:46 -0300

    Adicionar arquivos de projeto.

---
+++ ConversaoImplicitaCasting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+                float x = 4.5f;
+                double y = x;
+                Console.WriteLine(y);
+
+
+                double a;
+                float b;
+                a = 5.1;
+                b = (float)a;
+                Console.WriteLine(b);
+
+                double c;
+                int d;
+
+                c = 5.1;
+                d = (int)c;
+                Console.WriteLine(d);
+
+                int e = 5;
+                int f = 2;
+
+                double resultado = (double)e / f; 
+                Console.WriteLine(resultado);
+
+
+
+
+            }
+        }
+    }
+}
